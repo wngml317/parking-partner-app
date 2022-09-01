2167904f536acff973431eff27f5e204ed6ade83
Kdohyoung <asd62126310@gmail.com>
2022-09-01 12:29:43 +0900

    9-01

--- app/src/main/java/com/yh/parkingpartner/api/ApiFourthFragment.java
+++ app/src/main/java/com/yh/parkingpartner/api/ApiFourthFragment.java
@@ -1,10 +1,14 @@
 package com.yh.parkingpartner.api;
 
+import com.yh.parkingpartner.model.Data;
 import com.yh.parkingpartner.model.DataListRes;
+import com.yh.parkingpartner.model.Review;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface ApiFourthFragment {
@@ -12,5 +16,8 @@ public interface ApiFourthFragment {
 
     @GET("/parkingend/{prk_id}")
     Call<DataListRes> getPay(@Path("prk_id") int prk_id);
+
+    @PUT("/parkingend/{prk_id}")
+    Call<DataListRes> endParking(@Path("prk_id") int prk_id, @Body Data data);
 
 }
