4d0c4b8efde9396a8bcb618fdc8759bee63e96c0
kimyongjin77 <yjkim770518@gmail.com>
2022-09-02 16:11:26 +0900

    Update SecondFragment.java

--- app/src/main/java/com/yh/parkingpartner/ui/SecondFragment.java
+++ app/src/main/java/com/yh/parkingpartner/ui/SecondFragment.java
@@ -394,6 +394,9 @@ public class SecondFragment extends Fragment {
                 Toast.makeText(getContext(), "현재 입차한 주차장정보를 수신하지 못했습니다.", Toast.LENGTH_SHORT).show();
                 return;
             }
+            //주차구역 입력값 셋팅
+            data.setPrk_area(etxtArea.getText().toString());
+
             showProgress("저장 중 입니다...");
         }
 
